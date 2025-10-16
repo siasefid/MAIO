400683779d4bb2789fe56ccf9f48556ee3b78de6
siasefid <amirreza.sve@gmail.com>
2025-10-16 14:56:02 +0200

    here we go agian

--- src/risk_service/train.py
+++ src/risk_service/train.py
@@ -4,9 +4,9 @@ from risk_service.model import train_and_eval_models
 def main():
     X, y = load_dataset(as_frame=True)
     res = train_and_eval_models(X, y)
-print(f"RMSE: {res.rmse:.2f}")
-print(f"Training samples: {res.n_train}, Test samples: {res.n_test}")
-print(f"Saved model at: {res.model_path}")
+    print(f"RMSE: {res.rmse:.2f}")
+    print(f"Training samples: {res.n_train}, Test samples: {res.n_test}")
+    print(f"Saved model at: {res.model_path}")
 
 
 if __name__ == "__main__":
